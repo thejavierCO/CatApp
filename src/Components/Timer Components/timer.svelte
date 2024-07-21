2a95ae43a2d30ca3f8e42c080cf05d6747af27e8
Javier Cruz <31808656+thejavierCO@users.noreply.github.com>
2024-07-21 13:53:18 +0000

    idk

---
+++ src/Components/Timer Components/timer.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+  import type { ITime, IStatus } from "../../types";
+
+  import { createEventDispatcher, onMount } from "svelte";
+  import Counter from "./countInterval.svelte";
+  import { TimeView } from "../../js/data";
+  let emit = createEventDispatcher();
+
+  export let seconds: number = 1;
+  export let time: ITime = { start: 0, pause: 0, end: 0 };
+  export let autoRun = false;
+  export let status: IStatus = "Stop";
+
+  onMount(() => {
+    if (autoRun) status = "Play";
+  });
+</script>
+
+<Counter
+  on:current_status_timer={({ detail }) => {
+    if (status != detail.status) emit("state", detail);
+  }}
+  {status}
+  {seconds}
+  {time}
+  let:current_time
+  let:actions
+>
+  <slot
+    btnPause={() => actions.pause()}
+    btnStop={() => actions.stop()}
+    btnPlay={() => actions.play()}
+    {seconds}
+    {autoRun}
+    {status}
+    {time}
+    current_time={new TimeView(current_time)}
+  />
+</Counter>
