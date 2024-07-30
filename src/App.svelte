afc1952736fb23e729d3cea4b1489c0a6df0962c
Javier Cruz <31808656+thejavierCO@users.noreply.github.com>
2024-07-30 22:33:37 +0000

    ok

--- src/App.svelte
+++ src/App.svelte
@@ -10,7 +10,6 @@
 <Store
   let:edit
   let:del
-  let:store
   useLocalStorage
   on:mount={async ({ detail: { add, store } }) => {
     if (store().length === 0) {
@@ -25,41 +24,7 @@
     } else console.log("first");
   }}
 >
-  {#each store as { status, time, seconds, id, img }}
-    <Counter
-      autoRun
-      {seconds}
-      {status}
-      {time}
-      let:formatTime
-      on:state={({ detail }) => {
-        if (detail.status == "Stop") del(id);
-        edit(id, detail);
-      }}
-    >
-      <div>
-        <Background imageUrl={img} let:img>
-          <div class="flex justify-center">
-            <div class="rounded-lg shadow-lg bg-black max-w-sm grid">
-              <img
-                class="rounded-t-lg max-w-full h-auto items-center"
-                src={img}
-                alt="cat"
-              />
-              <span class="sm:text-sm md:text-2xl text-center text-white">
-                New Cat \(♥‿♥)/
-              </span>
-              <span class="text-center text-white">Delete in</span>
-              <span class="sm:text-sm md:text-2xl text-center text-white">
-                {formatTime.Hours}:{formatTime.Minutes}:{formatTime.Seconds}
-              </span>
-            </div>
-          </div>
-        </Background>
-      </div>
-    </Counter>
-  {/each}
-  <!-- <div slot="print" let:id let:data let:index>
+  <div slot="print" let:id let:data let:index>
     <Counter
       autoRun
       seconds={data.seconds}
@@ -92,5 +57,5 @@
         </Background>
       </div>
     </Counter>
-  </div> -->
+  </div>
 </Store>
