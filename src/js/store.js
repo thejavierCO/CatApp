0d26fbb7edff66e76a7559c6fc59809fbd3d6536
Javier Cruz <31808656+thejavierCO@users.noreply.github.com>
2024-08-01 02:07:36 +0000

    modify file store.js

--- src/js/store.js
+++ src/js/store.js
@@ -1,15 +1,8 @@
-import { writable, derived, get } from "svelte/store";
 import { CatApi } from './catApi';
 
-export let configApi = writable({})
-
-export let CatInfo = derived(configApi, async (e) => {
-  let cat = new CatApi("60213ae1-2505-46e8-8843-e72cc3b30468")
-  let json = await cat.images().search(e);
-  return json;
-})
-
-export let catImage = derived(CatInfo, async n => {
-  let [item] = await n;
-  return item.url;
-})
+export let catImage = async (configApi) => {
+  let cat = new CatApi("60213ae1-2505-46e8-8843-e72cc3b30468");
+  let json = await cat.images().search(configApi);
+  let [item] = await json;
+  return item.url
+}
